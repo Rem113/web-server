ad7845a988074a257c9b5157d6345e27915016c1
Hillel Saal <remi.saal@gmail.com>
2020-08-23 16:08:29 +0000

    Fixed GetPosts controller

--- src/controllers/post_controller.js
+++ src/controllers/post_controller.js
@@ -1,40 +1,36 @@
-const Post = require('../models/post')
+const Post = require("../models/post")
 
 const validateRegisterInput = ({ title, content }) => {
-    const errors = {}
+  const errors = {}
 
-    if (title === undefined || title === "")
-        errors.title = "Title cannot be empty"
+  if (title === undefined || title === "")
+    errors.title = "Title cannot be empty"
 
-    if (content === undefined || content === "")
-        errors.content = "Password cannot be empty"
+  if (content === undefined || content === "")
+    errors.content = "Password cannot be empty"
 
-    return Object.keys(errors).length > 0 ? errors : null
+  return Object.keys(errors).length > 0 ? errors : null
 }
 
 module.exports = {
-    PostPost: async (req, res) => {
-        const errors = validateRegisterInput(req.body)
+  PostPost: async (req, res) => {
+    const errors = validateRegisterInput(req.body)
 
-        if (errors !== null)
-            return res.status(400).json(errors)
+    if (errors !== null) return res.status(400).json(errors)
 
-        await Post.create({ ...req.body })
+    await Post.create(req.body)
 
-        return res.status(201).end()
-    },
+    return res.status(201).end()
+  },
 
-    GetPost: async (req, res) => {
-        Post.aggregate({},)
+  GetPosts: async (req, res) => {
+    const page = req.body.page - 1 || 0
 
+    const posts = await Post.find()
+      .sort({ postedAt: -1 })
+      .skip(page * 10)
+      .limit(10)
 
-        { }, (err, docs) => {
-            if (err) { console.error(err); return res.status(404) }
-            else {
-                console.log(docs)
-                return res.status(200).json(docs)
-            }
-        }
-    }
-
+    return res.status(200).json(posts)
+  },
 }
--- src/routes/post.js
+++ src/routes/post.js
@@ -1,9 +1,9 @@
 const { Router } = require("express")
-const PostController = require('../controllers/post_controller')
+const PostController = require("../controllers/post_controller")
 
-const router = Router();
+const router = Router()
 
-router.post('/', PostController.PostPost)
-router.get('/', PostController.GetPost)
+router.post("/", PostController.PostPost)
+router.get("/", PostController.GetPosts)
 
 module.exports = router
