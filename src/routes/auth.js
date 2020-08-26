699a869a8e4f2c953fd5152f327ea0a4b34b2ec4
Hillel Saal <remi.saal@gmail.com>
2020-08-26 20:10:53 +0000

    Added auth middleware to promote to manager

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,15 @@
 const { Router } = require("express")
 const AuthController = require("../controllers/auth_controller")
+const AuthMiddleware = require("../middlewares/auth_middleware")
 
 const router = Router()
 
 router.post("/register", AuthController.RegisterController)
 router.post("/login", AuthController.LoginController)
-router.put("/promote/to/manager", AuthController.PromoteToManager)
+router.put(
+  "/promote/to/manager",
+  AuthMiddleware,
+  AuthController.PromoteToManager
+)
 
 module.exports = router
