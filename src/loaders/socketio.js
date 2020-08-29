346efbf22c031fdd6c65d5088c28873dd1b2f58b
Gabriel <gelbaz@g.jct.ac.il>
2020-08-30 00:28:26 +0300

    Add Backup support

--- src/loaders/socketio.js
+++ src/loaders/socketio.js
@@ -3,13 +3,24 @@ const io = require("socket.io")
 module.exports = (http) => {
     const socket = io(http)
 
+    var messages = []
+
     socket.on("connection", (client) => {
+        console.log("💬 New user joined!")
         client.on("start", (user) => {
             client.broadcast.emit("info", { user, info: " has joined!" })
+            client.emit("back-up", messages)
 
             client.on("message", (message) => {
+                messages.push({ user, message })
                 socket.sockets.emit("message", { user, message })
             })
+
+            client.on("delete-backup", () => messages = [])
         })
+
+        client.on('disconnect', () => {
+            console.log('User disconnected');
+        });
     })
 }
