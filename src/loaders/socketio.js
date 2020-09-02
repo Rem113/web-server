4b6a7c6f1298174b10f3401e102edd0e2866dec7
Hillel Saal <remi.saal@gmail.com>
2020-09-02 16:11:58 +0000

    Updated chat

--- src/loaders/socketio.js
+++ src/loaders/socketio.js
@@ -1,26 +1,35 @@
 const io = require("socket.io")
 
 module.exports = (http) => {
-    const socket = io(http)
+  const socket = io(http)
 
-    var messages = []
+  var messages = []
 
-    socket.on("connection", (client) => {
-        console.log("💬 New user joined!")
-        client.on("start", (user) => {
-            client.broadcast.emit("info", { user, info: " has joined!" })
-            client.emit("back-up", messages)
+  socket.on("connection", (client) => {
+    client.on("start", ({ user, isManager }) => {
+      console.log(`💬 ${user} has joined!`)
+      client.broadcast.emit("info", {
+        date: new Date(),
+        user,
+        isManager,
+        info: " has joined",
+      })
+      client.emit("back-up", messages)
 
-            client.on("message", (message) => {
-                messages.push({ user, message })
-                socket.sockets.emit("message", { user, message })
-            })
+      client.on("message", (message) => {
+        const newMessage = { date: new Date(), user, isManager, message }
+        messages.push(newMessage)
+        socket.sockets.emit("message", newMessage)
+      })
 
-            client.on("delete-backup", () => messages = [])
+      client.on("disconnect", () =>
+        client.broadcast.emit("info", {
+          date: new Date(),
+          user,
+          isManager,
+          info: " has left",
         })
-
-        client.on('disconnect', () => {
-            console.log('User disconnected');
-        });
+      )
     })
+  })
 }
