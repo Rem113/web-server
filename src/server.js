cb9b3788e5a27ecd9a7d80c530fa0d3a72bd3f51
Hillel Saal <remi.saal@gmail.com>
2020-10-01 12:00:29 +0200

    Minor changes

--- src/server.js
+++ src/server.js
@@ -1,15 +1,15 @@
 require("dotenv").config()
 const express = require("express")
 const PORT = process.env.API_PORT
-const socketioLoader = require("./src/loaders/socketio")
-const expressLoader = require("./src/loaders/express")
-const mongoLoader = require("./src/loaders/mongoose")
+const socketioLoader = require("./loaders/socketio")
+const expressLoader = require("./loaders/express")
+const mongoLoader = require("./loaders/mongoose")
 
 const main = async () => {
   const app = express()
   const http = require("http").createServer(app)
   socketioLoader(http)
-  await expressLoader(app)
+  expressLoader(app)
   await mongoLoader()
   http.listen(PORT, () => console.log(`🚀 Server started on port ${PORT}`))
 }
